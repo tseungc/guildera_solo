ca79e8524822630c9c53bdfd09ff22147b9207d6
Colin <colin13@lpcuwc.edu.hk>
2025-04-27 16:13:29 +0800

    try debug

--- tutorial.js
+++ tutorial.js
@@ -319,7 +319,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('A_fighter2_button').classList.add('recruitedDisabled');
 		document.getElementById('A_archer1_button').classList.add('recruitedDisabled');
 //		document.getElementById('A_archer2_button').classList.add('recruitedDisabled');
-//		document.getElementById('A_wizard1_button').classList.add('recruitedDisabled');
+		document.getElementById('A_wizard1_button').classList.add('recruitedDisabled');
 //		document.getElementById('A_wizard2_button').classList.add('recruitedDisabled');
 	};
 
